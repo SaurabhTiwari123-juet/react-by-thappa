eecd220f6827b754df2603027ed4f6ba1b39804d
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2023-01-14 06:04:15 +0530

    channelge 1

---
+++ myapp/src/App.js
@@ -0,0 +1,39 @@
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className='App'>
+				<h1>this is heading</h1>
+				<p>the 5 items are as follows.</p>
+				<ol>
+					<li>nav</li>
+					<li>ddd</li>
+					<li>cccc</li>
+					<li>ffff</li>
+					<li>ddd</li>
+				</ol>
+
+				{/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+			</div>
+		</>
+	);
+}
+
+export default App;

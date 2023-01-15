db44815a62fe3f2c7fcd416ea6aa1fe3632d36c8
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2023-01-15 19:28:34 +0530

    channelge 1 completed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,12 +5,16 @@ import "./App.css";
 
 //render means to show and it always render one element only
 // so use react fragments <></>
+// we can only use the expression in {} not the statements
 function App() {
+	const name = "saurabh";
 	return (
 		<>
 			<div className='App'>
 				<h1>this is heading</h1>
 				<p>the 5 items are as follows.</p>
+				<p>the sum is {2 + 3}</p>
+				<p>my name is {name}</p>
 				<ol>
 					<li>nav</li>
 					<li>ddd</li>
